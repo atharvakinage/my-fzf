471c645d2a6dab9d2188d43600c7cf0f9ee24015
atharvakinage <kinageatharv@gmail.com>
2024-12-29 18:24:29 +0530

    Add condition to check for fzf command

--- fzf_navigate.ps1
+++ fzf_navigate.ps1
@@ -2,5 +2,14 @@
 
 # Write-Output "Hello"
 
+if(-not(Get-Command fzf -ErrorAction SilentlyContinue)){
+    Write-Error "fzf is not installed, install fzf first"
+    exit 1
+}
+else{
+    Write-Output "Allright good to go"
+}
+
+
 
 
