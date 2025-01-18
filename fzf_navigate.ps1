3b60f6cc2171f6007f118c0584298d4bbd2a76eb
atharvakinage <kinageatharv@gmail.com>
2025-01-18 17:40:12 +0530

    Handle file opening for multiple files when selected

--- fzf_navigate.ps1
+++ fzf_navigate.ps1
@@ -13,21 +13,21 @@ else{
 
 
 
-$selection = & {Get-ChildItem -Directory|
+$selection = & {Get-ChildItem |
     ForEach-Object { $_.FullName } |
     & fzf --multi --height=80% --border=sharp `
         --preview='tree {}' `
         --preview-window='45%,border-sharp' `
         --prompt='Dirs > ' `
-        --bind='del:execute(rm -ri {+})' `
+        --bind="del:execute()" `
         --bind='ctrl-p:toggle-preview' `
         --bind='ctrl-d:change-prompt(Dirs > )' `
-        --bind='ctrl-d:+reload:{}' `
+        --bind='ctrl-d:+reload(Get-ChildItem -Directory)' `
         --bind='ctrl-d:+change-preview(tree {})' `
         --bind='ctrl-d:+refresh-preview' `
         --bind='ctrl-f:change-prompt(Files > )' `
-        --bind='ctrl-f:+reload:{}' `
-        --bind='ctrl-f:+change-preview(cat {})' `
+        --bind='ctrl-f:+reload(ls | % { $_.FullName })' `
+        --bind='ctrl-f:+change-preview(tree /f {})' `
         --bind='ctrl-f:+refresh-preview' `
         --bind='ctrl-a:select-all' `
         --bind='ctrl-x:deselect-all' `
@@ -44,20 +44,25 @@ $selection = & {Get-ChildItem -Directory|
 
 
 # if directory then cd into it, if file then open using specified editor or by default vs code
-if (Test-Path -Path $selection -PathType Container){       
-    Set-Location -Path $selection   
-}
-elseif ($env:EDITOR) {
-    & $env:EDITOR $selection
-}
-else{
-    Write-Host "No editor defined - Opening with the vs code"
-    code $selection
-}
-
-
 # Container : eqivalent to -d in bash (directory) 
 # Leaf : equivalent to -f in bash (file)
 
-
-
+if ($selection) {
+    $selectionArray = $selection.Split("`n")
+    foreach ($path in $selectionArray) {
+        if (Test-Path -Path $path) {
+            if (Test-Path -Path $path -PathType Container) {
+                Set-Location -Path $path
+            }
+            elseif (Test-Path -Path $path -PathType Leaf) {
+                if ($env:EDITOR) {
+                    & $env:EDITOR $path
+                }
+                else {
+                    Write-Host "Opening $path with VS Code"
+                    code $path
+                }
+            }
+        }
+    }
+}
